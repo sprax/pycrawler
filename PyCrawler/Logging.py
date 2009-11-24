572f1bf8b51a0186ef5c4b70f356c877ff11c04e
jrf@mit.edu <jrf@mit.edu@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2009-11-24 15:40:47 +0000

    Delinting and preparing to replace with syslog.

--- PyCrawler/Logging.py
+++ PyCrawler/Logging.py
@@ -15,14 +15,14 @@ __version__ = "0.1"
 import os
 import sys
 import Queue
+import syslog
 import multiprocessing
 from time import sleep, time
 from signal import signal, alarm, SIGALRM, SIGHUP, SIGINT, SIGQUIT, SIGABRT, SIGTERM, SIGPIPE, SIG_IGN
 
-# global variable for start time; used with _log(..., relative=True)
-start = 0
+DELIMITER = "\t"
 
-syslog_type = None
+syslog_type = sys.stderr
 def syslog_open(name):
     """Given a name, does an openlog. Given a file handle, saves it for later writing."""
     global syslog_type
@@ -30,16 +30,11 @@ def syslog_open(name):
         syslog_type = name
     else:
         syslog.openlog(name, syslog.LOG_NDELAY|syslog.LOG_CONS|syslog.LOG_PID, syslog.LOG_LOCAL0)
+        syslog_type = None
 
-def log(msg):
-    """Log an error message, based on syslog_open"""
-    if syslog_type:
-        print >> syslog_type, msg
-        syslog_type.flush()
-    else:
-        syslog.syslog(syslog.LOG_ERR, msg)
-
-def _log(msg, fh=sys.stderr, timestamp=True, relative=True):
+# global variable for start time; used with log(..., relative=True)
+start = 0
+def log(msg, fh=syslog_type, timestamp=True, relative=True):
     """
     A low-level wrapper around the filehandle to which we are logging.
     Defaults to stderr.
@@ -49,8 +44,12 @@ def _log(msg, fh=sys.stderr, timestamp=True, relative=True):
         if relative:
             global start
             t -= start
-        msg = "%.2f\t%s" % (t, msg)
-    sys.stderr.write(msg)
+        msg = "%.2f%s%s" % (t, DELIMITER, msg)
+    if syslog_type:
+        syslog_type.write(msg)
+        syslog_type.flush()
+    else:
+        syslog.syslog(syslog.LOG_ERR, msg)
 
 def _construct_message(rec):
     """
@@ -63,17 +62,17 @@ def _construct_message(rec):
         if isinstance(field, (int, float)):    field =  str(field)
         if not isinstance(field, basestring): field = repr(field)
         rec_str.append(field)
-    return "\t".join(rec_str) + "\n"
+    return DELIMITER.join(rec_str) + "\n"
 
-def log(rec, level=1, verbosity=0):
+def maybelog(rec, level=1, verbosity=0):
     """
     If level is *below* the verbosity, then the rec gets passed to
-    _construct_message and logged with _log.  Note that this log
+    _construct_message and logged with log.  Note that this log
     method has rec first and it is not optional; contrast with
     ChangeLogger.log()
     """
     if level <= verbosity:
-        _log(_construct_message(rec))
+        log(_construct_message(rec))
 
 class Logger(multiprocessing.Process):
     """
@@ -85,7 +84,7 @@ class Logger(multiprocessing.Process):
 
     Logger.stop()
 
-    Logger.inQ.put( rec )
+    Logger.inQ.put(rec)
 
     We recommend not using put_nowait(), so that your process
     blocks if the Logger is backlogged.
@@ -100,7 +99,7 @@ class Logger(multiprocessing.Process):
         """
         multiprocessing.Process.__init__(self, name="Logger")
         for sig in (SIGALRM, SIGHUP, SIGINT, SIGQUIT, SIGABRT, SIGTERM):
-            signal( sig, SIG_IGN )
+            signal(sig, SIG_IGN)
         self.verbosity = verbosity
         self.inQ = multiprocessing.Queue()
         self.go  = multiprocessing.Event()
@@ -110,7 +109,7 @@ class Logger(multiprocessing.Process):
 
     def stop(self):
         "sleeps briefly to ensure inQ is empty before clearing self.go"
-        sleep( 0.3 )
+        sleep(0.3)
         self.go.clear()
 
     def run(self):
@@ -121,9 +120,9 @@ class Logger(multiprocessing.Process):
         # initial dotting state
         self.dotting = False
         self.pending = True
-        log( ("Logger", self.pid,
+        log(("Logger", self.pid,
               "entering logger loop; verbosity=%d" % self.verbosity),
-             0, 3 )
+             0, 3)
         sleep_interval = 0.5
         heartbeat_counter = 0
         heartbeat_interval = 10
@@ -137,7 +136,7 @@ class Logger(multiprocessing.Process):
                 self.pending = self.go.is_set()
                 heartbeat_counter += sleep_interval
                 if heartbeat_counter > heartbeat_interval:
-                    rec = ("alive",)
+                    rec = ("logger is alive after pause of %d seconds" % heartbeat_interval,)
                     level = 0
                 else:
                     continue
@@ -151,17 +150,17 @@ class Logger(multiprocessing.Process):
                 self.previous = msg
                 # if we were dotting, then we need to end the dots
                 if self.dotting:
-                    _log("\n", timestamp=False)
+                    log("\n", timestamp=False)
                     self.dotting = False
-                _log( msg )
+                log(msg)
             else:
                 self.dotting = True
-                _log(".", timestamp=False)
+                log(".", timestamp=False)
         # Since this process only consumes from this inQ, perhaps
         # there is something different we should do here?
         #self.inQ.close()
         #self.inQ.cancel_join_thread()
-        log( ("Logger", self.pid, "exiting",) , 0, 3 )
+        log(("Logger", self.pid, "exiting",) , 0, 3)
 
 class ChangeLogger:
     """
@@ -173,7 +172,7 @@ class ChangeLogger:
     the constructor of all ChangeLoggers.  This master instance then
     manages locking and dotting.
     """
-    def __init__( self, name="ChangeLogger", logger=None, verbosity=3 ):
+    def __init__(self, name="ChangeLogger", logger=None, verbosity=3):
         """
         Utility methods for logging.
 
@@ -190,7 +189,7 @@ class ChangeLogger:
             # start it immediately, because it must be a child of the
             # main process for the inQ to work (in current versions of
             # multiprocessing)
-            logger = Logger( verbosity )
+            logger = Logger(verbosity)
             logger.go.set()
             logger.start()
             self._am_master_logger = True
@@ -199,12 +198,12 @@ class ChangeLogger:
         self.logger = logger
         self._log_func = lambda step: step
 
-    def stop( self ):
+    def stop(self):
         """If this instance created the logger, then stop it."""
         if self._am_master_logger:
             self.logger.stop()
 
-    def set_msg_func( self, func ):
+    def set_msg_func(self, func):
         """
         Set a function to create log messages for this ChangeLogger
         instance.  func must accept an optional 'step' parameter and
@@ -216,7 +215,7 @@ class ChangeLogger:
         """
         self._log_func = func
 
-    def log( self, level=1, msg=None, step=None ):
+    def log(self, level=1, msg=None, step=None):
         """
         If msg is absent, then it calls the function provided to
         set_msg_func to get a msg.
@@ -230,27 +229,27 @@ class ChangeLogger:
         """
         assert level <= 3, "We want to define only three levels of verbosity."
         if msg is None: 
-            msg = self._log_func( step )
-        if hasattr( self, "pid" ):
+            msg = self._log_func(step)
+        if hasattr(self, "pid"):
             pid = self.pid
         else:
             pid = os.getpid()
-        rec = ( self.name, pid, msg )
-        self.logger.inQ.put( ( rec, level ) )
+        rec = (self.name, pid, msg)
+        self.logger.inQ.put((rec, level))
 
-class ChangeLoggerContainer( ChangeLogger, multiprocessing.Process ):
+class ChangeLoggerContainer(ChangeLogger, multiprocessing.Process):
     """
     A basic subclass of Process and ChangeLogger that has a self.go
     Event
     """
-    def __init__( self, name="ChangeLoggerContainer", 
-                  go=None, logger=None, verbosity=3 ):
-        ChangeLogger.__init__( self, name, logger, verbosity )
-        multiprocessing.Process.__init__( self, name=name )
+    def __init__(self, name="ChangeLoggerContainer", 
+                  go=None, logger=None, verbosity=3):
+        ChangeLogger.__init__(self, name, logger, verbosity)
+        multiprocessing.Process.__init__(self, name=name)
         self.go = go or multiprocessing.Event()
         self.go.set()
 
-    def run( self ):
+    def run(self):
         """
         
         """
@@ -259,64 +258,72 @@ class ChangeLoggerContainer( ChangeLogger, multiprocessing.Process ):
         while self.go.is_set():
             sleep(1)
 
-    def stop( self, a=None, b=None): 
+    def stop(self, a=None, b=None): 
         """
         Clears the go Event on ChangeLoggerContainer instances that have
         their own go event (rather than receiving it on instantiation).
         This function can be called by signal.signal
         """
-        self.log( 2, "stop( %s )" % a )
+        self.log(2, "stop(%s)" % a)
         if self.go is not None: self.go.clear()
-        ChangeLogger.stop( self )
+        ChangeLogger.stop(self)
 
 if __name__ == "__main__":
-    class Test( ChangeLoggerContainer ):
-        def run( self ):
-            self.set_msg_func( 
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("--syslog", dest="syslog", default=False, action="store_true", help="run tests with logging to syslog instead of stderr")
+    parser.add_option("--delimiter", dest="delimiter", default="|", help="string to use for delimiting fields within a message")
+    (options, args) = parser.parse_args()
+    if options.syslog:
+        syslog_open("pycrawler")
+    DELIMITER = options.delimiter    
+    class Test(ChangeLoggerContainer):
+        def run(self):
+            self.set_msg_func(
                 lambda step: "count=%s, go=%s" % \
-                    (self.count, self.go.is_set() ) )
-            self.log( 0, "starting" )
+                    (self.count, self.go.is_set()))
+            self.log(0, "starting")
             self.count = 0
             while self.go.is_set():
-                self.log( 0 )
-                sleep( .1 )
+                self.log(0)
+                sleep(.1)
                 self.count += 1
                 if self.count == 1:
                     for a in range(100):
-                        self.log( 0 )
-            self.log( 0, "stopping" )
+                        self.log(0)
+            self.log(0, "stopping")
             self.logger.stop()
 
-    t1 = Test( "test1" )
-    t2 = Test( "test2", t1.go, t1.logger )
+    t1 = Test("test1")
+    t2 = Test("test2", t1.go, t1.logger)
 
-    def mlog( msg ):
-        log( ("__main__", msg), level=0, verbosity=3 )
+    def mlog(msg):
+        maybelog(("__main__", msg), level=0, verbosity=3)
 
     def stop(a, b):
-        mlog( "received %s" % a )
+        mlog("received %s" % a)
         t1.stop()
 
     for sig in (SIGALRM, SIGHUP, SIGINT, SIGQUIT, SIGABRT, SIGTERM):
-        signal( sig, stop )
-    alarm( 1 )
+        signal(sig, stop)
+    alarm(1)
 
     mlog("about to start")
     t1.start()
     t2.start()
 
-    while len( multiprocessing.active_children() ) > 0:
+    while len(multiprocessing.active_children()) > 0:
         sleep(1)
-        mlog( "waiting for children to stop before joining..." )
+        mlog("waiting for children to stop before joining...")
 
     # If we don't do the while loop above, then this first join
     # (always?) throws an error about interupted system call.
     t1.join()
-    mlog( "t1.exitcode = %s" % t1.exitcode )
-    mlog( "joined 1, waiting for second..." )
+    mlog("t1.exitcode = %s" % t1.exitcode)
+    mlog("joined 1, waiting for second...")
 
     t2.join()
-    mlog( "joined 2" )
-    mlog( "t2.exitcode = %s" % t2.exitcode )
+    mlog("joined 2")
+    mlog("t2.exitcode = %s" % t2.exitcode)
 
     print "If you got here without any tracebacks or having to kill it, then the test passed."
