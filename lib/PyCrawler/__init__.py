373e2b5c5d4b53e585462f6b9e2532151d132101
postshift@gmail.com <postshift@gmail.com@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2009-12-04 01:16:12 +0000

    moving data path into config

--- lib/PyCrawler/__init__.py
+++ lib/PyCrawler/__init__.py
@@ -15,7 +15,7 @@ __license__ = "MIT License"
 __version__ = "0.1"
 
 import URL
-from Server import FetchServer, FetchClient, PORT, AUTHKEY, DATA_PATH
+from Server import FetchServer, FetchClient, PORT, AUTHKEY
 from Fetcher import Fetcher, URLinfo, HostSummary
 from AnalyzerChain import AnalyzerChain, Analyzer, GetLinks, LogInfo, SpeedDiagnostics
 from TextProcessing import is_text
