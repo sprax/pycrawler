559129e0d5d6b1ebeebc27cad6c76972b928e3f7
postshift@gmail.com <postshift@gmail.com@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2009-12-26 18:16:15 +0000

    illustrates how to use queues in multiprocessing

---
+++ lib/queues/tests/test_multiprocessing_queue.py
@@ -0,0 +1,42 @@
+import os
+from time import sleep
+import multiprocessing
+import multiprocessing.managers
+from syslog import syslog
+
+class Process(multiprocessing.Process):
+    # create our own subclass of SyncManager
+    class Manager(multiprocessing.managers.SyncManager): pass
+
+    def __init__(self):
+        multiprocessing.Process.__init__(self)
+
+        self.go = multiprocessing.Event()
+        self.go.set()
+ 
+    def run(self):
+        # instantiate the manager and get it started
+        self.manager = self.Manager()
+        self.manager.start()
+
+        while self.go.is_set():
+            try:
+                syslog("Got: %s" % str(self.inQ.get_nowait()))
+            except Queue.Empty:
+                sleep(1)
+                continue
+            except Exception, exc:
+                syslog("Hit problem: %s" % traceback.format_exc(exc))
+        syslog("exiting run")
+
+    def stop(self):
+        syslog("stopping, pid: %d" % os.getpid())
+        self.go.clear()
+
+syslog("creating process, pid: %d" % os.getpid())
+p = Process()
+p.start()
+
+sleep(1)
+
+p.stop()
