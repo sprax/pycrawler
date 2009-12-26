1e6056cf45298064cc777ce98c515d76ae9f99f7
postshift@gmail.com <postshift@gmail.com@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2009-12-26 19:58:37 +0000

    adding the all-important __init__ file that makes this a package

---
+++ lib/queues/PersistentQueue/__init__.py
@@ -0,0 +1,15 @@
+"""
+PersistentQueue provides queue and heap-like interfaces to a set of
+flat files stored on disk.
+"""
+# $Id: $
+__copyright__ = "Copyright 2009, John R. Frank"
+__credits__ = ["Kjetil Jacobsen"]
+__license__ = "MIT License"
+__version__ = "0.1"
+__maintainer__ = "John R. Frank"
+
+import LineFiles
+from PersistentQueue import PersistentQueue as Queue
+from PersistentQueue import NotYet
+from TriQueue import TriQueue, Blocked, Syncing, ReadyToSync
