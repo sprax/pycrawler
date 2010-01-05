c12abffbbcb335c70af2cd98017e6a99e143d4b2
postshift@gmail.com <postshift@gmail.com@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-01-05 12:16:33 +0000

    Adding notion of a group-then-sort to TriQueue, which is essentially a second sorting that is managed by a second marshal.  If sorting_marshal is passed into the constructor then the Merger does two sorts:  first the marshal, and then the sorting_marshal.
    hostsQ uses a sorting_marshal to sort by next_time after the regular marshal is used
    to group the rows by hostkey for de-duplication.

--- lib/queues/PersistentQueue/TriQueue.py
+++ lib/queues/PersistentQueue/TriQueue.py
@@ -42,12 +42,36 @@ class TriQueue:
     class Syncing(Exception): pass
     class Blocked(Exception): pass
 
-    def __init__(self, data_path, marshal=nameddict):
+    def __init__(self, data_path, marshal=nameddict, sorting_marshal=None):
+        """
+        Open or create a TriQueue at the file system location
+        'data_path'.  The three internal PersistentQueue instances
+        will be subdirectories.
+
+        'marshal' must be a subclass of nameddict, which is used to
+        stringify the data in the queues.  This is used in the
+        constructor to each of the PersistentQueue instances.
+
+        If sorting_marshal is not None, then it is used instead of
+        'marshal' in _readyQ.  This also causes a *second* sorting to
+        occur during the merge process initiated by a call to sync().
+        After the _inQ and _pendingQ are merged into _readyQ using the
+        _sort_key defined by 'marshal', the _readyQ is sorted a second
+        time using its marshal.  This means that the sorting_marshal
+        must differ only slight from 'marshal', typically this means
+        it differs only in the value of its _sort_key property.
+        """
         self._data_path = data_path
         self._inQ_path = os.path.join(data_path, "_inQ")
         self._readyQ_path = os.path.join(data_path, "_readyQ")
         self._pendingQ_path = os.path.join(data_path, "_pendingQ")
         self._marshal = marshal
+        if sorting_marshal is not None:
+            self._sorting_marshal = sorting_marshal
+            self._second_sort = True
+        else:
+            self._sorting_marshal = marshal
+            self._second_sort = False
         self._inQ = None
         self._readyQ = None
         self._pendingQ = None
@@ -60,17 +84,23 @@ class TriQueue:
         self._sync_pending = Mutex(
             self._sync_pending_mutex_path)
 
-    def _open_queues(self):
+    def __str__(self):
+        "provide lengths of each internal queue"
+        return "inQ(%d) readyQ(%d) pendingQ(%d) %s syncing" \
+            % (len(self._inQ), len(self._readyQ), len(self._pendingQ),
+               self._sync_pending.available() and "NOT" or "IS")
+
+    def _open_queues(self, protect_readyQ=False):
         """
         Open the three queues
         """
-        self._inQ = PersistentQueue(self._inQ_path, marshal=self._marshal, unlocked=True)
-        self._readyQ = PersistentQueue(self._readyQ_path, marshal=self._marshal, unlocked=True)
-        self._pendingQ = PersistentQueue(self._pendingQ_path, marshal=self._marshal, unlocked=True)
+        self._inQ =        PersistentQueue(self._inQ_path,      marshal=self._marshal, singleprocess=True)
+        self._pendingQ =   PersistentQueue(self._pendingQ_path, marshal=self._marshal, singleprocess=True)
+        self._readyQ =     PersistentQueue(self._readyQ_path,   marshal=self._sorting_marshal, singleprocess=not protect_readyQ)
         
     def close(self):
         """
-        Close all three queues that we have openned.
+        Close all three queues that we have opened.
         """
         self._mutex.acquire()
         self._inQ.close()
@@ -126,9 +156,13 @@ class TriQueue:
         except NormalQueue.Empty:
             if not self._sync_pending.available():
                 raise self.Syncing
-            else:
-                self._readyQ.make_singleprocess_safe()
-            #syslog("in the mix: %d %d %d %s" % (len(self._inQ), len(self._readyQ), len(self._pendingQ), self._data_path))
+            elif not self._readyQ.only_singleprocess():
+                # reopen readyQ with singleprocess=True
+                self._readyQ.close()
+                self._readyQ = PersistentQueue(
+                    self._readyQ_path,   
+                    marshal=self._sorting_marshal, 
+                    singleprocess=True)
             if (len(self._inQ) + len(self._pendingQ)) > 0:
                 # only ReadyToSync when readyQ is Empty and the other
                 # two queues are not Empty
@@ -165,55 +199,69 @@ class TriQueue:
         # This is inside the mutex.acquire, so no other process
         # can get confused about whether we're syncing.
         self._sync_pending.release()
+        assert len(self._readyQ) == 0, "sync() requires empty _readyQ"
         # self._mutex is acquired, so put/get will block while we
         # rename directories and setup new versions of queues
         self._inQ.close()
-        self._readyQ.close()
+        self._readyQ.close()  # will be reopened without change below
         self._pendingQ.close()
         # create temp paths for syncing, so can give to Merger below
-        _inQ_syncing = self._inQ_path + "_syncing"
-        _readyQ_syncing = self._readyQ_path + "_syncing"
-        _pendingQ_syncing = self._pendingQ_path + "_syncing"
+        _inQ_syncing_path = self._inQ_path + "_syncing"
+        _pendingQ_syncing_path = self._pendingQ_path + "_syncing"
         # move the dirs
-        shutil.move(self._inQ_path, _inQ_syncing)
-        shutil.move(self._readyQ_path, _readyQ_syncing)
-        shutil.move(self._pendingQ_path, _pendingQ_syncing)
+        shutil.move(self._inQ_path, _inQ_syncing_path)
+        shutil.move(self._pendingQ_path, _pendingQ_syncing_path)
         #syslog(LOG_DEBUG, "recreating three Queues as empty")
         #map(lambda line: syslog(LOG_NOTICE, line), os.listdir(self._data_path))
-        self._open_queues()
-        self._readyQ.make_multiprocess_safe()
+        self._open_queues(protect_readyQ=True)
         # while still holding the mutex, we launch a process to
         # sort and merge all the files.  The child process acquires
         # the sync_pending mutex, which we checked above.
         class Merger(multiprocessing.Process):
             "manages the merge"
             name = "MergerProcess"
-            _marshal = self._marshal
-            _paths = [_inQ_syncing, _readyQ_syncing, _pendingQ_syncing]
-            _sync_pending_mutex_path = self._sync_pending_mutex_path
-            _readyQ = self._readyQ
             _debug = self._debug
+            _TriQueue = self
+            _paths = [_inQ_syncing_path, _pendingQ_syncing_path]
             def run(self):
                 "waits for merge to complete"
                 try:
                     openlog(self.name, LOG_NDELAY|LOG_CONS|LOG_PID, LOG_LOCAL0)
                     if not self._debug:
                         setlogmask(LOG_UPTO(LOG_INFO))
-                    self._sync_pending = Mutex(self._sync_pending_mutex_path)
+                    self._sync_pending = Mutex(self._TriQueue._sync_pending_mutex_path)
                     self._sync_pending.acquire()
-                    pq = PersistentQueue(self._paths[0], marshal=self._marshal)
-                    queues = [PersistentQueue(self._paths[1], marshal=self._marshal),
-                              PersistentQueue(self._paths[2], marshal=self._marshal)] 
+                    inQ_syncing      = PersistentQueue(self._paths[0], marshal=self._TriQueue._marshal)
+                    pendingQ_syncing = PersistentQueue(self._paths[1], marshal=self._TriQueue._marshal)
                     start = time()
-                    retval = pq.sort(
-                        merge_from=queues, 
-                        merge_to=self._readyQ)
+                    if not self._TriQueue._second_sort:
+                        # this uses self._marshal's accumulator
+                        retval = inQ_syncing.sort(
+                            merge_from=[pendingQ_syncing],
+                            merge_to=self._TriQueue._readyQ)
+                    else:
+                        # merge into self, will resort and merge into readyQ below
+                        retval = inQ_syncing.sort(merge_from=[pendingQ_syncing])
                     end = time()
-                    syslog(LOG_INFO, "sort finished in %.1f seconds" % (end - start))
+                    syslog(LOG_INFO, "merge took %.1f seconds" % (end - start))
                     assert retval is True, \
                         "Should get True from sort, instead: " + str(retval)
+                    if self._TriQueue._second_sort:
+                        # close temp queue and reopen with new marshal
+                        inQ_syncing.close()
+                        start = time()
+                        inQ_syncing = PersistentQueue(
+                            self._paths[0], 
+                            marshal=self._TriQueue._sorting_marshal,
+                            singleprocess=True)
+                        inQ_syncing.sort(merge_to=self._TriQueue._readyQ)
+                        end = time()
+                        syslog(LOG_INFO, "second sort took %.1f seconds" % (end - start))
+                        assert retval is True, \
+                            "Should get True from sort, instead: " + str(retval)
                     # if sort fails, the following will not happen
-                    pq.close()
+                    inQ_syncing.close()
+                    pendingQ_syncing.close()
                     for path in self._paths:
                         shutil.rmtree(path)
                     self._sync_pending.release()
@@ -235,11 +283,12 @@ class TriQueue:
         self._mutex.release()
         return merger
 
-    def sync_and_close(self):
+    def close_spawn_sync_and_close(self):
         """
         Starts a child process that syncs this TriQueue and then
         closes it.
         """
+        self.close()
         class SyncAndClose(multiprocessing.Process):
             name = "SyncAndClose: %s" % self._data_path
             _TriQueue = self
@@ -247,8 +296,9 @@ class TriQueue:
                 "calls sync and waits for it to finish before closing"
                 try:
                     openlog(self.name, LOG_NDELAY|LOG_CONS|LOG_PID, LOG_LOCAL0)
+                    self._TriQueue._open_queues()
                     self._TriQueue.sync()
-                    while not self._TriQueue.sync_pending.available():
+                    while not self._TriQueue._sync_pending.available():
                         sleep(2)
                     self._TriQueue.close()
                     syslog(LOG_DEBUG, "Done syncing and closing")
