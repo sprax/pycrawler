0e86141911597f04c7ef47407d1b75ec03790cc8
postshift@gmail.com <postshift@gmail.com@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-01-07 02:58:18 +0000

    playing with Zope Corp Dict which can be persistent in a ZODB... not going to use this

---
+++ lib/queues/tests/test_zc_dict.py
@@ -0,0 +1,30 @@
+"""
+Simple tests of the speed of Zope Corp's persistent dictionaries,
+which use BTree.
+"""
+import transaction
+from time import time
+from ZODB import FileStorage, DB
+from zc.dict import Dict
+
+big_state = Dict()
+
+num = 10**9
+start = time()
+for i in range(num):
+    big_state[i] = "boom"
+end = time()
+elapsed = end - start
+print "inserted %d records in %.2f seconds: %.2f rec/sec" % (num, elapsed, num / elapsed)
+
+start = time()
+storage = FileStorage.FileStorage('ZODB-test.fs')
+db = DB(storage)
+connection = db.open()
+root = connection.root()
+root["big_state"] = big_state
+transaction.manager.commit()
+connection.close()
+end = time()
+elapsed = end - start
+print "saved %d records in %.2f seconds: %.2f rec/sec" % (num, elapsed, num / elapsed)
