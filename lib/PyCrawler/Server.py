e00b4381f206543b28680af3d017d2a4401a40d3
postshift@gmail.com <postshift@gmail.com@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-01-19 05:14:40 +0000

    Simplifying by moving the functionality of Server into CrawlStateManager.

--- lib/PyCrawler/Server.py
+++ lib/PyCrawler/Server.py
@@ -11,12 +11,7 @@ __author__ = "John R. Frank"
 __copyright__ = "Copyright 2009, John R. Frank"
 __license__ = "MIT License"
 __version__ = "0.1"
-"""
-TODO
 
-Actually finish this --- this does not work yet; not done.
-
-"""
 import os
 import Queue
 import daemon  # from PyPI
@@ -29,51 +24,10 @@ from time import time, sleep
 from syslog import syslog, LOG_INFO, LOG_DEBUG, LOG_NOTICE
 from Fetcher import Fetcher
 from Process import Process, multi_syslog
-from AnalyzerChain import FetchInfo
 from CrawlStateManager import CrawlStateManager
-
-# using older than python2.6...
-import ctypes
-def floatfromhex(my_hex):
-    my_int = int(my_hex, 16)                              # convert from hex to a Python int
-    cp = ctypes.pointer(ctypes.c_int(my_int))             # make this into a c integer
-    fp = ctypes.cast(cp, ctypes.POINTER(ctypes.c_float))  # cast the int pointer to a float pointer
-    return fp.contents.value         # dereference the pointer, get the float
 
 PORT = 18041 # default port is the second prime number above 18000
 AUTHKEY = "APPLE"
-
-class Sender(Process):
-    def __init__(self, go, inQ, id, hostbins, authkey=AUTHKEY):
-        self.name = "Sender:%s" % id
-        Process.__init__(self, go)
-        self.inQ = inQ
-        self.hostbins = hostbins
-        self.authkey = authkey
-
-    def run(self):
-        """
-        uses FetchClient to connect to each FetchServer's
-        hostname:port specified as the values of hostbins, and
-        sends the data that this FetchServer has accumulated for
-        that other FetchServer
-        """
-        syslog(LOG_DEBUG, "Starting.")
-        try:
-            self.prepare_process()
-            while self._go.is_set():
-                try:
-                    fetch_info = self.inQ.get_nowait()
-                except Queue.Empty:
-                    sleep(1)
-                fmc = FetchServerClient(
-                    self.hostbins.get_fetch_server(fetch_info.hostbin), 
-                    self.authkey)
-                fmc.put(fetch_info)
-                fmc.close()
-        except Exception, exc:
-            multi_syslog(traceback.format_exc(exc))
-        syslog(LOG_DEBUG, "Exiting.")
 
 class FetchServer(Process):
     class ManagerClass(multiprocessing.managers.BaseManager): pass
@@ -101,22 +55,19 @@ class FetchServer(Process):
         self.relay = mgr.Namespace()
         self.config = None
         self.inQ = multiprocessing.Queue(1000)
-        self.outQ = None
         self.packerQ = None
         self.ManagerClass.register("put", callable=self.inQ.put)
         self.ManagerClass.register("stop", callable=self.stop)
         self.ManagerClass.register("set_config", callable=self.set_config)
 
     def set_config(self, config):
-        """
-        Passes config into the relay
-        """
+        "Passes config into the relay"
         try:
             self.relay.config = config
             self.reload.set()
+            syslog("set_config")
         except Exception, exc:
-            syslog(traceback.format_exc(exc))
-        syslog("finished calling reload.set()")
+            multi_syslog(exc)
 
     def run(self):
         """
@@ -132,46 +83,38 @@ class FetchServer(Process):
 
             0) handle reload event
 
-            1) get files from receiver, hand them to CrawlStateManager
+            1) launch CrawlStateManager
 
-            2) launch Fetcher on highest scoring fraction of URLs in
-            hostbins assigned to this FetchServer
-
-            3) launch Sender to send hostbins assigned to other
-            FetchServers
-
-            4) wait for fetchers and sender to finish
-
+            2) launch Fetcher(s) to consume from packerQ
         """
         try:
             self.prepare_process()
             self.manager = self.ManagerClass(self.address, self.authkey)
             self.manager.start()
-            # make a queue for passing records to sender
-            self.outQ = multiprocessing.Queue(1000)
             self.packerQ = multiprocessing.Queue(1000)
             syslog(LOG_DEBUG, "Entering main loop")
             while self._go.is_set():
                 if self.reload.is_set():
                     if self.valid_new_config():
                         self.config = copy(self.relay.config)
-                        self.start_children()
+                        syslog(LOG_DEBUG, "creating & starting CrawlStateManager")
+                        self.csm = CrawlStateManager(
+                            self._go, self.id, self.inQ, self.packerQ, self.config)
+                        self.csm.start()
                     self.reload.clear()
                 if self.config is None:
                     syslog(LOG_DEBUG, "Waiting for config")
                     sleep(1)
                     continue
-                # Get an AnalyzerChain. This allows csm to record data
-                # as it streams out of fetcher.  The config could
-                # cause csm to add more Analyzers to the chain.
+                # AnalyzerChain records data streaming out of fetchers
                 ac = self.csm.get_analyzerchain()
                 while self._go.is_set() and not self.reload.is_set():
                     syslog(LOG_DEBUG, "Creating & start Fetcher")
                     # could do multiple fetchers here...
                     self.fetcher = Fetcher(
                         go = self._go,
+                        hostQ = self.hostQ,
                         outQ = ac.inQ,
-                        packerQ = self.packerQ,
                         params = self.config["fetcher_options"])
                     self.fetcher.start()
                     while self._go.is_set() and self.fetcher.is_alive():
@@ -192,25 +135,6 @@ class FetchServer(Process):
                 syslog("Waiting for: " + str(multiprocessing.active_children()))
                 sleep(1)
             syslog("Exiting FetchServer.")
-
-    def start_children(self):
-        """
-        Creates a CrawlStateManager and a Sender using self.config
-        """
-        # csm handles disk interactions with state
-        syslog(LOG_DEBUG, "creating & starting CrawlStateManager")
-        self.csm = CrawlStateManager(
-            self._go, self.id, self.inQ, self.outQ, self.packerQ, self.config)
-        self.csm.start()
-        # launch self.Sender to send hostbins assigned to other
-        # FetchServers
-        self.sender = Sender(
-            self._go,
-            self.outQ,
-            self.id,
-            self.config["hostbins"],
-            authkey = self.authkey)
-        self.sender.start()
 
     def valid_new_config(self):
         """returns bool indicating whether 'config' is valid, i.e. has
