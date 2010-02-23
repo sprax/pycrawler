1e2461152cb00244bd2dfc86b3651aaf8dff3025
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-02-23 22:11:05 +0000

    setup path for tests to run properly with nosetests

---
+++ lib/pycrawler/tests/__init__.py
@@ -0,0 +1,14 @@
+
+def setUpModule():
+
+    import sys
+    import os.path
+
+    # i.e. ~/pycrawler_checkout/lib/pycrawler
+    path = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+    queues = os.path.join(os.path.split(path)[0], 'queues')
+
+    sys.path.insert(0, queues)
+    sys.path.insert(0, path)
+
+setUpModule()
