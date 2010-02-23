e81d51c04b68803387c188e86d32a4ddf41e52f3
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-02-23 22:11:27 +0000

    nosify test_robots.py

--- lib/pycrawler/tests/test_robots.py
+++ lib/pycrawler/tests/test_robots.py
@@ -1,17 +1,23 @@
+
+import os.path
 import cPickle as pickle
 import robotparser
 
-data = open("robots.txt").read()
-print "%d bytes data" % len(data)
+def test_robots():
+    path = os.path.split(os.path.abspath(__file__))[0]
+    data = open(os.path.join(path, "robots.txt")).read()
+    print "%d bytes data" % len(data)
 
-rp = robotparser.RobotFileParser()
-print "%d pickled bytes of robotparser" % len(pickle.dumps(rp))
+    rp = robotparser.RobotFileParser()
+    print "%d pickled bytes of robotparser" % len(pickle.dumps(rp))
 
-rp.parse(data.splitlines())
-assert not rp.can_fetch("", "/search")
+    rp.parse(data.splitlines())
+    assert not rp.can_fetch("", "/search")
 
-print "%d pickled bytes of robotparser" % len(pickle.dumps(rp))
+    print "%d pickled bytes of robotparser" % len(pickle.dumps(rp))
 
-#print pickle.dumps(rp)
+    #print pickle.dumps(rp)
 
 
+if __name__ == '__main__':
+    test_robots()
