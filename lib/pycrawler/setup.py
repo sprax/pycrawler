765c137f5c9cedcd3baf6a1318c8abe180b201b3
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-03-05 17:42:14 +0000

    setup.py for PyCrawler to install dependencies.

---
+++ lib/pycrawler/setup.py
@@ -0,0 +1,35 @@
+# $Id$
+__copyright__ = "Copyright 2009, John R. Frank"
+__license__ = "MIT License"
+__version__ = "0.1"
+__maintainer__ = "John R. Frank"
+
+from setuptools import setup
+
+version = "0.1"
+
+setup(name="PyCrawler",
+      version=version,
+      description="A python-based distributed web crawler.",
+      long_description="""
+Provides a web crawler.
+""",
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        ],
+      keywords="python, crawler",
+      author="John R. Frank",
+      author_email="postshift gmail",
+      url="http://code.google.com/p/pycrawler/",
+      license="MIT",
+      zip_safe=False,
+      install_requires=['pycurl', 'PersistentQueue', 'daemon', 'simplejson'],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      packages=["PyCrawler"],
+      )
+
