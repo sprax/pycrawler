f59a81d15127c93a32dee7706508d3662362083c
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-03-12 23:40:12 +0000

    require version 0.11 of nose, since we use it.

--- lib/pycrawler/setup.py
+++ lib/pycrawler/setup.py
@@ -28,7 +28,7 @@ Provides a web crawler.
       zip_safe=False,
       install_requires=['pycurl', 'PersistentQueue', 'daemon', 'simplejson'],
 
-      setup_requires=['coverage', 'nose'],
+      setup_requires=['coverage', 'nose>=0.11'],
 
       entry_points="""
       # -*- Entry points: -*-
