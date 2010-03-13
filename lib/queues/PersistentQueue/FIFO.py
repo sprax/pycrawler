3980f53961614c690a57f46a8c70ac9e45333ff9
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-03-13 01:09:14 +0000

    split FIFO into a superclass that does most of the operations, but has exception-raising stubs for all IO-related functions, and a subclass that implements the IO-related things, so that we can easily test a mocked-out FIFO queue.

--- lib/queues/PersistentQueue/FIFO.py
+++ lib/queues/PersistentQueue/FIFO.py
@@ -8,12 +8,15 @@ __license__ = "MIT License"
 __version__ = "0.1"
 __maintainer__ = "John R. Frank"
 import os
+import errno
 import Queue
 from blist import blist
 from Mutex import Mutex
 
-class FIFO:
+class BaseFIFO(object):
     """
+    Abstract base class for FIFO.
+
     Provides a queue-like interface to a set of flat files.  Records
     are must already be serialized into strings, which are appended to
     the flat files as new lines.
@@ -24,33 +27,16 @@ class FIFO:
 
         cache_size determines maximum number of items per file.
         """
+
+        # cache size must be >= 1, otherwise won't work.
+        assert cache_size >= 1
+
         self._cache_size = cache_size
-        self._index_file = os.path.join(data_path, "index")
-        self._temp_path  = os.path.join(data_path, "tempfile")
-        self._data_path  = os.path.join(data_path, "data")
-        if not os.path.exists(self._data_path):
-            try:
-                os.makedirs(self._data_path)
-            except OSError, exc:
-                # okay if someone else just made it:
-                if exc.errno == 17: pass
-                else: raise
-        # acquire mutex to protect on-disk state files
-        self._mutex = Mutex(os.path.join(data_path, "lock_file"))
-        acquired = self._mutex.acquire(block=False)
-        if not acquired:
-            raise Exception("failed to acquire mutex: %s" % self._data_path)
+
         # setup in-memory state
         self._head = 0
         self._tail = 1
-        # if index exists, reset them
-        if os.path.exists(self._index_file):
-            index_file = open(self._index_file)
-            index_data = index_file.read()
-            index_file.close()
-            # last step in try/except sets the pointers
-            self._head, self._tail = \
-                map(int, index_data.split(" "))
+
         # now setup in-memory caches
         self._head_cache = None
         self._tail_cache = None
@@ -58,34 +44,32 @@ class FIFO:
         self._load_cache("_tail_cache", self._tail)
 
     def _flush_index(self):
+
         """
         Ensures data stored in index file matches in-memory state
         represented by self._head and self._tail
         """
-        index_file = open(self._index_file, "w")
-        index_file.write("%d %d" % (self._head, self._tail))
-        index_file.close()
+        with self._index_file(mode='w') as index_file:
+            index_file.write("%d %d" % (self._head, self._tail))
 
     def _flush_cache(self, cache, num):
         """
         Writes 'cache' to a file named by 'num'
         """
-        path = os.path.join(self._data_path, str(num))
-        fh = open(path, "wb")
-        fh.write("\n".join(cache))
-        fh.close()
+        with self._cache_file(num, mode='w') as fh:
+            fh.write("\n".join(cache))
 
     def _load_cache(self, cache_name, num):
         """
         Reads lines from file named by 'num' into cache_name, which is
         either _head_cache or _tail_cache
         """
-        path = os.path.join(self._data_path, str(num))
-        if os.path.exists(path):
-            fh = open(path, "rb")
-            cache = blist(fh.read().splitlines())
-            fh.close()
-        else:
+        try:
+            with self._cache_file(num) as fh:
+                cache = blist(fh.read().splitlines())
+        except IOError, e:
+            if e.errno != errno.ENOENT:
+                raise
             cache = blist()
         setattr(self, cache_name, cache)
 
@@ -131,9 +115,7 @@ class FIFO:
                 self._load_cache("_head_cache", next_head)
                 # remove previous file, because its elements were
                 # returned by previous calls to get(0
-                old_head = os.path.join(self._data_path, str(self._head))
-                if os.path.exists(old_head):
-                    os.remove(old_head)
+                self._drop_cache(self._head)
                 # update head position: it moves one up (see above)
                 self._head = next_head
             # make sure head is always less than tail:
@@ -194,4 +176,66 @@ class FIFO:
         self.sync()
         self._head_cache = None
         self._tail_cache = None
+
+    def _index_file(self, mode='r'):
+        raise Exception("not implemented")
+
+    def _cache_file(self, num, mode='r'):
+        raise Exception("not implemented")
+
+    def _drop_cache(self, num):
+        raise Exception("not implemented")
+
+class FIFO(BaseFIFO):
+    def __init__(self, data_path, cache_size=2**16):
+        """
+        Create a FIFO of flat files in data_path/ directory.
+
+        cache_size determines maximum number of items per file.
+        """
+
+        self._index_filename = os.path.join(data_path, "index")
+        self._temp_path  = os.path.join(data_path, "tempfile")
+        self._data_path  = os.path.join(data_path, "data")
+
+        if not os.path.exists(self._data_path):
+            try:
+                os.makedirs(self._data_path)
+            except OSError, exc:
+                # okay if someone else just made it:
+                if exc.errno == 17: pass
+                else: raise
+        # acquire mutex to protect on-disk state files
+        self._mutex = Mutex(os.path.join(data_path, "lock_file"))
+        acquired = self._mutex.acquire(block=False)
+        if not acquired:
+            raise Exception("failed to acquire mutex: %s" % self._data_path)
+
+        super(FIFO, self).__init__(cache_size=cache_size)
+
+        # if index exists, reset them
+        if os.path.exists(self._index_filename):
+            index_file = open(self._index_filename)
+            index_data = index_file.read()
+            index_file.close()
+            # last step in try/except sets the pointers
+            self._head, self._tail = \
+                map(int, index_data.split(" "))
+
+    def _index_file(self, mode='r'):
+        assert mode in ('r', 'w')
+        return open(self._index_filename, mode)
+
+    def _cache_file(self, num, mode='r'):
+        assert mode in ('r', 'w')
+        path = os.path.join(self._data_path, str(num))
+        return open(path, mode + 'b')
+
+    def _drop_cache(self, num):
+        old_head = os.path.join(self._data_path, str(num))
+        if os.path.exists(old_head):
+            os.remove(old_head)
+
+    def close(self):
+        super(FIFO, self).close()
         self._mutex.release()
