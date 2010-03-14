d32d4c7e3bcc5a4b017e8636db65d9c6d6ab0ac3
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-03-14 02:12:59 +0000

    move test inside __name__ == '__main__'

--- lib/queues/tests/test_recordfifo.py
+++ lib/queues/tests/test_recordfifo.py
@@ -23,41 +23,43 @@ def rmdir(dir):
         except Exception, exc:
             print "Did not rmtree the dir. " + str(exc)
 
-parser = OptionParser()
-parser.add_option("-n", "--num", type=int, default=5, dest="num")
-parser.add_option("-c", "--cache_size", type=int, default=5, dest="cache_size")
-(options, args) = parser.parse_args()
-num = options.num
-cache_size = options.cache_size
-
-test_dir = "test_dir"
-rmdir(test_dir)
-
 MyRec = define_record("MyRec", ("next", "score", "depth", "data", "hostkey", "foo", "dog"))
 def get_fifo():
     "make a factory for testing"
-    return RecordFIFO(
+    return RecordFIFO.RecordFIFO(
         MyRec,
         (int, float, int, b64, Static("http://www.wikipedia.com"), Static(None), JSON),
         test_dir, cache_size=cache_size,
         defaults = {"next": 0, "score": 0, "data": "did we survive b64ing?", "dog": {}})
 
-start = time()
-fifo = get_fifo()
-for i in xrange(num):
-    fifo.put(**{"depth": i})
-fifo.close()
-elapsed_put = time() - start
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-n", "--num", type=int, default=5, dest="num")
+    parser.add_option("-c", "--cache_size", type=int, default=5, dest="cache_size")
+    (options, args) = parser.parse_args()
+    num = options.num
+    cache_size = options.cache_size
 
-start = time()
-fifo = get_fifo()
-for i in xrange(num):
-    val = fifo.get()
-    assert val.depth == i, "\n\nval: %s != %s" % (val, str(i))
-fifo.close()
-elapsed_get = time() - start
+    test_dir = "test_dir"
+    rmdir(test_dir)
 
-files = os.listdir("test_dir/data/")
-assert len(files) == 2, str(files)
-rmdir(test_dir)
-print "%.1f put/sec, %.1f get/sec" % (num / elapsed_put, num / elapsed_get)
+
+    start = time()
+    fifo = get_fifo()
+    for i in xrange(num):
+        fifo.put(**{"depth": i})
+    fifo.close()
+    elapsed_put = time() - start
+
+    start = time()
+    fifo = get_fifo()
+    for i in xrange(num):
+        val = fifo.get()
+        assert val.depth == i, "\n\nval: %s != %s" % (val, str(i))
+    fifo.close()
+    elapsed_get = time() - start
+
+    files = os.listdir("test_dir/data/")
+    assert len(files) == 2, str(files)
+    rmdir(test_dir)
+    print "%.1f put/sec, %.1f get/sec" % (num / elapsed_put, num / elapsed_get)
