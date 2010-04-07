c3463410d3db8b43bc4fa332b7c0d9a893a18875
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-04-07 18:15:46 +0000

    get rid of unused string record definition code.

--- lib/queues/PersistentQueue/record.py
+++ lib/queues/PersistentQueue/record.py
@@ -96,8 +96,7 @@ def define_record(typename, field_names):
     """
     # Parse and validate the field names.  Validation serves two purposes,
     # generating informative error messages and preventing template injection attacks.
-    if isinstance(field_names, basestring):
-        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
+
     field_names = tuple(map(str, field_names))
     for name in (typename,) + field_names:
         if not all(c.isalnum() or c=='_' for c in name):
