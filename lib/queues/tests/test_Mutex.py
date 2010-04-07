202bbe074f731a9c66cb877dec26f5c79bc35f7c
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-04-07 18:45:09 +0000

    use assert_equal from nose in more places for better assert error messages, and fix test now that Mutex always acquires on releases.

--- lib/queues/tests/test_Mutex.py
+++ lib/queues/tests/test_Mutex.py
@@ -2,6 +2,8 @@
 import tempfile
 from PersistentQueue import Mutex
 
+from nose.tools import assert_equal
+
 class TestMutex():
     def acquire_callback(self, *args, **kwargs):
         assert not args and not kwargs
@@ -21,26 +23,32 @@ class TestMutex():
                   acquire_callback = self.acquire_callback,
                   release_callback = self.release_callback)
 
-        assert self.acquired == self.released == 0
+        assert_equal(self.acquired, 1)
+        assert_equal(self.released, 1)
+
+        self.acquired = 0
+        self.released = 0
 
         assert m.available() == True
 
-        assert self.acquired == 0, "Acquired is %d, but should be 0" % self.acquired
-        assert self.released == 0
+        assert_equal(self.acquired, 0)
+        assert_equal(self.released, 0)
 
         assert m.acquire() == True
 
-        assert self.acquired == 1, "Acquired is %d, but should be 1" % self.acquired
-        assert self.released == 0
+        assert_equal(self.acquired, 1)
+        assert_equal(self.released, 0)
 
         m.release()
-        assert self.acquired == 1, "Acquired is %d, but should be 1" % self.acquired
-        assert self.released == 1
+
+        assert_equal(self.acquired, 1)
+        assert_equal(self.released, 1)
 
         # Test multiple releases
         m.release()
-        assert self.acquired == 1, "Acquired is %d, but should be 1" % self.acquired
-        assert self.released == 1
+
+        assert_equal(self.acquired, 1)
+        assert_equal(self.released, 1)
 
     def test_multiple_locks(self):
         s = tempfile.NamedTemporaryFile()
@@ -56,22 +64,30 @@ class TestMutex():
                    acquire_callback = self.acquire_callback,
                    release_callback = self.release_callback)
 
+        self.acquired = 0
+        self.released = 0
+
         assert m1.available() == m2.available() == True
 
         assert m1.acquire() == True
 
+        assert_equal(self.acquired, 1)
+        assert_equal(self.released, 0)
+
         assert m2.available() == False
 
         assert m2.acquire(block=False) == False
-        assert self.acquired == 1
-        assert self.released == 0
+
+        assert_equal(self.acquired, 1)
+        assert_equal(self.released, 0)
 
         assert m1.available() == False
         assert m2.available() == False
 
         assert m1.release() is None
-        assert self.acquired == 1
-        assert self.released == 1
+
+        assert_equal(self.acquired, 1)
+        assert_equal(self.released, 1)
 
         assert m1.available() == True
 
