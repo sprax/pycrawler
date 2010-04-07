981f18fc461f11316478f2db3cd61e628bdcf33d
jmalicki <jmalicki@b6364aeb-bb51-0410-a87d-35e5c2c85351>
2010-04-07 18:26:39 +0000

    get fetcher test working again.

--- lib/pycrawler/tests/test_fetcher.py
+++ lib/pycrawler/tests/test_fetcher.py
@@ -18,7 +18,7 @@ import multiprocessing
 import time
 from optparse import OptionParser
 
-from PyCrawler import Fetcher, FetchInfoFIFO
+from PyCrawler import Fetcher, FetchInfo
 
 from nose.tools import assert_false
 from nose.exc import DeprecatedTest
@@ -47,39 +47,20 @@ class TestFetcher:
             if e.errno != errno.ENOENT:
                 raise
 
-    def test_fetcher(self, num=5, timeout=20):
+    def test_fetcher(self, timeout=20):
         """ Test that fetcher can download several URLs as specified. """
         # make factories for creating surrogate HostRecord and RawFetchRecords for testing:
 
-        raise DeprecatedTest("The URL loading is temporarily broken")
+        num = 1
+        url = 'http://www.google.com/robots.txt'
+        f = FetchInfo.create(url)
 
-        # make temp copy of url_parts fifo and make records for all items in the FIFO
-        shutil.copytree(self.url_parts_dir,
-                        self.temp_dir)
-
-        f = FetchInfoFIFO(self.temp_dir)
-        hosts = {}
-        count = 0
-        for u in f:
-            count += 1
-            if count > num: break
-            if u.hostname not in hosts:
-                hosts[u.hostname] = host_factory.create(**{"hostname": u.hostname})
-            hosts[u.hostname].data["links"].append(
-                fetch_rec_factory.create(**u.__getstate__()))
-            fetchrec = hosts[u.hostname].data["links"][-1]
-            logging.info('Enqueued http://%s%s' % (fetchrec.hostname, fetchrec.relurl))
-        f.close()
-        del f
-
-        hostQ = multiprocessing.Queue(len(hosts.keys()))
+        inQ = multiprocessing.Queue(1)
         outQ = multiprocessing.Queue()
-        for hostname in hosts:
-            hostQ.put(hosts[hostname])
 
-        logging.info('%d (of %s) items enqueued.' % (len(hosts.keys()), num))
+        inQ.put(f)
 
-        fetcher = Fetcher(hostQ=hostQ, outQ=outQ, _debug=True)
+        fetcher = Fetcher(inQ=inQ, outQ=outQ, _debug=True)
         fetcher.start()
 
         t1 = time.time()
